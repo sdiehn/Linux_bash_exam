e00f3100ce1d2a86cb6e53d52f1d2b62b84abbf9
Ubuntu <ubuntu@ip-172-31-20-41.eu-west-1.compute.internal>
2025-11-03 17:44:31 +0000

    mit model_list...again

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -1,30 +1,16 @@
-# ==============================================================================
-# Script : collect.sh
-# Description :
-#   Ce script interroge une API afin de récupérer les ventes des modèles de cartes graphiques suivants :
-#     - rtx3060
-#     - rtx3070
-#     - rtx3080
-#     - rtx3090
-#     - rx6700
-#
-#   Les données collectées sont ajoutées à une copie du fichier :
-#     data/raw/sales_data.csv
-#
-#   Le fichier de sortie est sauvegardé au format :
-#     data/raw/sales_YYYYMMDD_HHMM.csv
-#   avec les colonnes suivantes :
-#     timestamp, model, sales
-#
-#   L’activité de collecte (requêtes, modèles interrogés, résultats, erreurs)
-#   est enregistrée dans un fichier de log :
-#     logs/collect.logs
-#
-#   Le log est lisible et doit inclure :
-#     - La date et l’heure de chaque requête
-#     - Les modèles interrogés
-#     - Les ventes récupérées
-#     - Les éventuelles erreurs
-# ==============================================================================
+#!/bin/bash
 
+API_URL="http://0.0.0.0:5000"
 
+timestamp=$(date "+%Y%m%d_%H%M")
+filename="../data/raw/sales_${timestamp}.csv"
+touch "$filename"
+echo "Timestamp,Model,Sales" >> "$filename"
+
+while read model;
+do
+    sales=$(curl  "${API_URL}/${model}")
+    echo "${timestamp},${model},${sales}" >> "$filename"
+
+echo "[$timestamp]: Model=${model}, Sales= ${sales}" >> ../logs/collect.logs
+done < ../data/model_list.txt
